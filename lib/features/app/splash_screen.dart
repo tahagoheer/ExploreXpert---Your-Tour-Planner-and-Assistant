b164a7eb30ac87f6c8f87a935969086b399542fb
tahagoheer <tahaahmadmuhammad@gmail.com>
2024-04-07 13:42:24 +0500

    A Tour Planner and Virtual Assistant Flutter App (Android, IOS, Web)

--- lib/features/app/splash_screen.dart
+++ lib/features/app/splash_screen.dart
@@ -11,7 +11,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushAndRemoveUntil(
           context,
           MaterialPageRoute(builder: (context) => widget.child!),

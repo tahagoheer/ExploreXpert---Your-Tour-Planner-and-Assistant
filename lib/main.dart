435509753bf672a6287c4a96458a22a6cafb04f8
Muhammad Taha Ahmad <tahaahmadmuhammad@gmail.com>
2024-04-29 01:05:48 +0500

    A Tour Planner and Virtual Assistant Flutter App (Android, IOS, Web)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:explorexpert/features/app/splash_screen.dart';
 import 'package:explorexpert/features/user_auth/presentation/pages/auth_via_page.dart';
-<<<<<<< HEAD
-import 'package:explorexpert/features/user_auth/presentation/pages/profile_setup_page.dart';
-=======
->>>>>>> 0ad9d5624835cb3008e40b0f6b1646b993d43116
 import 'package:explorexpert/features/user_auth/presentation/widgets/essentials.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -23,19 +19,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-        debugShowCheckedModeBanner: false,
-        title: 'ExploreXpert',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: EXColors.primaryDark),
-          useMaterial3: true,
-        ),
-        home: const ProfileSetupPage()
-        // const SplashScreen(
-        //   child: AuthViaPage(),
-        // ),
-        );
-=======
       debugShowCheckedModeBanner: false,
       title: 'ExploreXpert',
       theme: ThemeData(
@@ -46,6 +29,5 @@ class MyApp extends StatelessWidget {
         child: AuthViaPage(),
       ),
     );
->>>>>>> 0ad9d5624835cb3008e40b0f6b1646b993d43116
   }
 }
